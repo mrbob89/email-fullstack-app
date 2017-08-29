3a6296fa85d6188f24d6203144aa72ca778aa44b
Roman Boboshko <romanboboshko89@gmail.com>
2017-08-30 00:45:52 +0300

    Add new route

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
+const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
 
 const app = express();
 
@@ -10,20 +10,22 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
+      callbackURL: "/auth/google/callback"
     },
-    accessToken => {
+    (accessToken, refreshToken, profile, done) => {
       console.log(accessToken);
     }
   )
 );
 
 app.get(
-  '/auth/google',
-  passport.authenticate('google', {
-    scope: ['profile', 'email']
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
   })
 );
+
+app.get("/auth/google/callback", passport.authenticate("google"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
