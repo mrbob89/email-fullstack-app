b3bd0934239a637ce6821f34c9138e0702827b65
Roman Boboshko <romanb@compandsave.com>
2017-09-01 16:09:51 +0300

    Update client side, add actions, add proxy

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,7 +2,18 @@ import React, { Component } from 'react';
 
 class Header extends Component {
   render() {
-    return <div>Header</div>;
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <a className="brand-logo">
+            Email App
+          </a>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li><a>Login With Google</a></li>
+          </ul>
+        </div>
+      </nav>
+    )
   }
 }
 
